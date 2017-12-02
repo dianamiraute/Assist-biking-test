ef643205701849ed27182d4c2164a4d73cafa7f3
dianamiraute <diana.miraute@gmail.com>
2017-12-02 20:40:47 +0200

    Update project

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -9,3 +9,55 @@ window.$ = $;
 import hello from './hello';
 
 hello();
+
+
+// -------------------------------------------------------------------------------------
+// MOBILE MENU
+//--------------------------------------------------------------------------------------
+function toggleMenu() {
+  $('.js-c-page-nav__menu').toggleClass('active');
+  $('.js-c-page-nav__toggle').toggleClass('active'); 
+}
+
+$('.js-c-page-nav__toggle').on('click', toggleMenu);
+  
+
+
+
+// -------------------------------------------------------------------------------------
+// NAV MENU SCROLL
+//--------------------------------------------------------------------------------------
+
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 50) {
+    $('.js-c-page-nav').addClass('shrink');
+    $('.js-c-page-nav__logo-img').addClass('shrink');
+    $('.js-c-page-nav__toggle-img').addClass('shrink');
+  } else {
+    $('.js-c-page-nav').removeClass('shrink');
+    $('.js-c-page-nav__logo-img').removeClass('shrink');
+    $('.js-c-page-nav__toggle-img').removeClass('shrink');
+  }
+});
+
+
+
+
+
+$(function() {
+  
+  // contact form animations
+  $('.js-c-team-member__popup').click(function() {
+    $('.c-team-popup').fadeToggle();
+  })
+  $(document).mouseup(function (e) {
+    var container = $(".c-team-popup");
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.fadeOut();
+    }
+  });
+  
+});
